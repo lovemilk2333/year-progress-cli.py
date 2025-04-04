e8225e233428c495f3bbc4bba9424493f8caef86
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-04-05 05:58:03 +0800

    同步主项目 v2.0 所做的提示更改和命令行参数改动

--- year-progress.py
+++ year-progress.py
@@ -6,12 +6,15 @@ import time
 parser = argparse.ArgumentParser(
     prog="year-progress.py", description="Display how many days you lost this year", allow_abbrev=False
 )
-parser.add_argument("--version", "-v", action="version", version="develop", help="display version info")
-parser.add_argument("--max-width", type=int, help="the progress bar width")
+parser.add_argument("--version", "-v", action="version", version="develop", help="Display version info")
+parser.add_argument("--width", type=int, help="The progress bar width")
 args = parser.parse_args()
 
-if args.max_width != None:
-    print_width = args.max_width
+if args.width != None:
+    print_width = args.width
+    if print_width < 3:
+        print("Error: Width < 3")
+        exit(1)
 else:
     print_width = int(0.6 * 80)
 
@@ -25,19 +28,15 @@ year_ratio = tm.tm_yday / days_year
 year_percent = year_ratio * 100
 
 print(
-    "This year has lost ",
+    "It's day ",
     tm.tm_yday,
-    "/",
+    " of the year(",
     days_year,
-    " days. That's already ",
-    format(year_percent, ".3f"),
+    "). That's already ",
+    format(year_percent, ".1f"),
     "%",
     sep="",
 )
-
-if print_width <= 3:
-    print("Error: Width <= 3")
-    exit(1)
 
 print("[", end="")
 
